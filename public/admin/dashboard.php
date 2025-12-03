bcbfedba4c55e9a8d1ef4c3c96541b06d69d8a24
Rockstany <albinsoftware1996@gmail.com>
2025-12-03 09:44:17 +0530

    First Time Project Push

--- public/admin/dashboard.php
+++ public/admin/dashboard.php
@@ -245,7 +245,7 @@ $recentActivity = $stmt->fetchAll();
                 <li><a href="/public/admin/dashboard.php" style="font-weight: 600;">Dashboard</a></li>
                 <li><a href="/public/admin/users.php">Manage Users</a></li>
                 <li><a href="/public/admin/communities.php">Manage Communities</a></li>
-                <li><a href="/public/admin/system-health.php">🖥️ System Health</a></li>
+                <li><a href="/public/admin/system-health.php">System Health</a></li>
                 <li><a href="/public/admin/change-password.php">Change Password</a></li>
                 <li><a href="/public/logout.php">Logout</a></li>
             </ul>
