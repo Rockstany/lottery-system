a810c4952ba6153891e1a8e97d26a3a00ac4185d
Rockstany <albinsoftware1996@gmail.com>
2025-12-04 23:17:33 +0530

    First Time Project Push

--- public/group-admin/lottery-book-reassign.php
+++ public/group-admin/lottery-book-reassign.php
@@ -4,6 +4,11 @@
  * GetToKnow Community App
  */
 
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../../config/config.php';
 AuthMiddleware::requireRole('group_admin');
 
